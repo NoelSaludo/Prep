12cac09c7bf16f11bd3a59150b6f88e10ef0801b
NoelSaludo <23-08992@g.batstate-u.edu.ph>
2025-11-20 22:36:57 +0800

    Implement login and registration functionality in LoginController with corresponding routes and tests

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
+/* Route::get('/', [LoginController::class, 'showLoginForm'])->name('login'); */
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/login', [LoginController::class, 'login']);
+Route::get('/register', [LoginController::class, 'showRegisterForm'])->name('register');
+Route::post('/register', [LoginController::class, 'register']);
+Route::post('/app', function () {
+    return response()->noContent();
+})->middleware('auth');
